a429dd3339698008163854e1303b5b3ae3252839
Vedernikov <vedernikovnv@gmail.com>
2021-02-24 18:27:54 +0300

    Checked

--- java-solutions/queue/ArrayQueue.java
+++ java-solutions/queue/ArrayQueue.java
@@ -23,24 +23,24 @@ public class ArrayQueue {
    Pred: obj != null
    Post: n = n' + 1 && a[n] = obj && forall i=1...n' a[i] == a'[i]
     */
-    public void enqueue(ArrayQueue this, Object obj) {
+    public void enqueue( Object obj) {
         Objects.requireNonNull(obj);
-        this.ensureCapacity(size + 1);
-        this.elements[(head + size++) % elements.length] = obj;
+        ensureCapacity(size + 1);
+        elements[(head + size++) % elements.length] = obj;
     }
 
     /*
     Pred: capacity >= 0
     Post: elements.length >= capacity, если хотим добавить не больше чем в 2 раза && immutable
      */
-    private void ensureCapacity(ArrayQueue this, final int capacity) {
+    private void ensureCapacity( final int capacity) {
         assert capacity >= 0;
-        if (this.elements.length < capacity) {
+        if (elements.length < capacity) {
             Object[] tmp = new Object[capacity * 2];
-            System.arraycopy(this.elements, this.head, tmp, 0, this.elements.length - this.head);
-            System.arraycopy(this.elements, 0, tmp, this.elements.length - this.head, this.head);
-            this.elements = tmp;
-            this.head = 0;
+            System.arraycopy(elements, head, tmp, 0, elements.length - head);
+            System.arraycopy(elements, 0, tmp, elements.length - head, head);
+            elements = tmp;
+            head = 0;
         }
     }
 
@@ -48,22 +48,22 @@ public class ArrayQueue {
     Pred: n > 0
     Post: R = a'[1] && Immutable
     */
-    public Object element(ArrayQueue this) {
-        assert this.size > 0;
-        return this.elements[this.head % this.elements.length];
+    public Object element() {
+        assert size > 0;
+        return elements[head];
     }
 
     /*
    Pred: n > 0
    Post: R = a'[1] && n = n'-1 && forall i = 1..n: a[i] = a'[i+1]
    */
-    public Object dequeue(ArrayQueue this) {
-        assert this.size > 0;
-        this.size--;
-        Object result = this.elements[this.head % this.elements.length];
+    public Object dequeue() {
+        assert size > 0;
+        size--;
+        Object result = elements[head];
 
-        this.elements[this.head % this.elements.length] = null;
-        this.head = (this.head + 1) % this.elements.length;
+        elements[head] = null;
+        head = (head + 1) % elements.length;
         return result;
     }
 
@@ -71,45 +71,45 @@ public class ArrayQueue {
     Pred: True
     Post: R = n && Immutable
      */
-    public int size(ArrayQueue this) {
-        return this.size;
+    public int size() {
+        return size;
     }
 
     /*
     Pred: True
     Post: R = (n == 0) && Immutable
      */
-    public boolean isEmpty(ArrayQueue this) {
-        return this.size == 0;
+    public boolean isEmpty() {
+        return size == 0;
     }
 
     /*
     Pred: True
     Post: n = 0
      */
-    public void clear(ArrayQueue this) {
-        this.elements = new Object[2];
-        this.size = 0;
-        this.head = 0;
+    public void clear() {
+        elements = new Object[2];
+        size = 0;
+        head = 0;
     }
 
     /*
-    Pred: i >= 0
-    Post: R = a[i] && Immutable
+    Pred: n > i >= 0
+    Post: R = a[i + 1] && Immutable
      */
-    public Object get(ArrayQueue this, int i) {
+    public Object get(int i) {
         assert i >= 0;
-        return this.elements[(this.head + i) % this.elements.length];
+        return elements[(head + i) % elements.length];
     }
 
     /*
-    Pred: i >= 0 && obj != null
-    Post: a[i] = obj && n = n' && forall k = 1..n\{i} a[k] = a[k']
+    Pred: n > i >= 0 && obj != null
+    Post: a[i + 1] = obj && n = n' && forall k = 1..n\{i} a[k] = a[k']
      */
-    public void set(ArrayQueue this, int i, Object obj) {
+    public void set(int i, Object obj) {
         assert i >= 0;
         Objects.requireNonNull(obj);
-        this.elements[(this.head + i) % this.elements.length] = obj;
+        elements[(head + i) % elements.length] = obj;
     }
 
 }
