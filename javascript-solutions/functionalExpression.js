38c9cf48819a68fdda40aab9e3a7460d59c48a53
Dzhakhbarov_Vladimir <dzahbarovvladimir@gmail.com>
2021-03-24 18:26:38 +0300

    HW5 fixed bugs

--- javascript-solutions/functionalExpression.js
+++ javascript-solutions/functionalExpression.js
@@ -2,22 +2,15 @@
 // Binary Operations
 let binary = (f, lhs, rhs) => (x, y, z) => f(lhs(x, y, z), rhs(x, y, z));
 
-let addImpl = (lhs, rhs) => lhs + rhs;
-let subImpl = (lhs, rhs) => lhs - rhs;
-let mulImpl = (lhs, rhs) => lhs * rhs;
-let divImpl = (lhs, rhs) => lhs / rhs;
-
-let add = (lhs, rhs) => binary(addImpl, lhs, rhs);
-let subtract = (lhs, rhs) => binary(subImpl, lhs, rhs);
-let multiply = (lhs, rhs) => binary(mulImpl, lhs, rhs);
-let divide = (lhs, rhs) => binary(divImpl, lhs, rhs);
+let add = (lhs, rhs) => binary((lhs, rhs) => lhs + rhs, lhs, rhs);
+let subtract = (lhs, rhs) => binary((lhs, rhs) => lhs - rhs, lhs, rhs);
+let multiply = (lhs, rhs) => binary((lhs, rhs) => lhs * rhs, lhs, rhs);
+let divide = (lhs, rhs) => binary((lhs, rhs) => lhs / rhs, lhs, rhs);
 
 // Unary Operations
 let unary = (f, element) => (x, y, z) => f(element(x, y, z));
 
-let negateImpl = element => -element;
-
-let negate = element => unary(negateImpl, element);
+let negate = element => unary(element => -element, element);
 
 let cnst = (value) => () => value;
 
